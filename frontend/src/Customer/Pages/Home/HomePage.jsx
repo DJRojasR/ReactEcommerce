f89581a4f5959f85fcced7ec7cdafeeb3e2edcde
Darwin Rojas <josteinrojas590@gmail.com>
2025-04-22 00:42:28 -0500

    Primer commit con estructura del frontend

---
+++ frontend/src/Customer/Pages/Home/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../Home/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarousel/>
+        <div>
+            otra seccion
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
